74055d6c8cbbfa551522e1f9e905e41bc3026b19
David Ash <bottegadash@gmail.com>
2020-05-16 14:07:25 -0400

    Added first set of skews

--- src/components/app.js
+++ src/components/app.js
@@ -3,8 +3,13 @@ import React, { Component } from 'react';
 export default class App extends Component {
   render() {
     return (
-      <div className='app'>
-        <h1>Birthday Countdown</h1>
+      <div className='grid'>
+        <div>hi</div>
+        <div>hi</div>
+        <div>hi</div>
+        <div className="grid_skew-light-one"></div>
+        <div className="grid_skew-light-two"></div>
+        <div className="grid_skew-light-three-box"></div>
       </div>
     );
   }
