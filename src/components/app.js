f43a26fdfd6f7508340ef5c627109a41829afb78
David Ash <bottegadash@gmail.com>
2020-05-16 14:30:04 -0400

    placed dark skew and refactored SCSS

--- src/components/app.js
+++ src/components/app.js
@@ -4,9 +4,8 @@ export default class App extends Component {
   render() {
     return (
       <div className='grid'>
-        <div>hi</div>
-        <div>hi</div>
-        <div>hi</div>
+        <div className="grid_skew-dark-one"></div>
+
         <div className="grid_skew-light-one"></div>
         <div className="grid_skew-light-two"></div>
         <div className="grid_skew-light-three-box"></div>
