05eaddf2e302d343cd8396b676df7f18305061cc
David Ash <bottegadash@gmail.com>
2020-05-17 02:24:30 -0400

    styled picker

--- src/components/picker.js
+++ src/components/picker.js
@@ -1,6 +1,8 @@
 import React, {Component} from "react";
 import DatePicker from "react-datepicker";
 
+import "react-datepicker/dist/react-datepicker.css";
+
 class Picker extends Component {
 
     constructor (props) {
